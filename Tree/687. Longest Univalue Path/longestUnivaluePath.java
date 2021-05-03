22b4c20ccc86b17aaf38a00a0e982bc8f2180c6c
xxpp1314 <37097790+xxpp1314@users.noreply.github.com>
2021-05-02 23:52:31 -0700

    Create longestUnivaluePath.java

---
+++ Tree/687. Longest Univalue Path/longestUnivaluePath.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int len = 0; // global variable
+    public int longestUnivaluePath(TreeNode root) {
+        if (root == null) return 0;
+        len = 0;
+        getLen(root, root.val);
+        return len;
+    }
+
+    private int getLen(TreeNode node, int val) {
+        if (node == null) return 0;
+        int left = getLen(node.left, node.val);
+        int right = getLen(node.right, node.val);
+        len = Math.max(len, left + right);
+        if (val == node.val)  return Math.max(left, right) + 1;
+        return 0;
+    }
+}
