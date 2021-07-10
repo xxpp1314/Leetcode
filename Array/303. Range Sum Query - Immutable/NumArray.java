110623e74e1e8f884f4f88ab406160205be14c10
xxpp1314 <37097790+xxpp1314@users.noreply.github.com>
2021-07-09 23:26:40 -0700

    Create NumArray.java

---
+++ Array/303. Range Sum Query - Immutable/NumArray.java
@@ -0,0 +1,26 @@
+public class NumArray {
+
+    int[] nums;
+
+    public NumArray(int[] nums) {
+        for(int i = 1; i < nums.length; i++)
+            nums[i] += nums[i - 1];
+
+        this.nums = nums;
+    }
+
+    public int sumRange(int i, int j) {
+        if(i == 0)
+            return nums[j];
+
+        return nums[j] - nums[i - 1];
+    }
+}
+//https://leetcode.com/problems/range-sum-query-immutable/discuss/75192/Java-simple-O(n)-init-and-O(1)-query-solution
+
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
