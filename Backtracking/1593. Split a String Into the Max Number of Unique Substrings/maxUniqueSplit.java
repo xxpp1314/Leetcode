951891526a01b9babc68a94759acd21e8b5532cb
xxpp1314 <37097790+xxpp1314@users.noreply.github.com>
2021-07-15 21:10:06 -0700

    Create maxUniqueSplit.java

---
+++ Backtracking/1593. Split a String Into the Max Number of Unique Substrings/maxUniqueSplit.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxUniqueSplit(String s) {
+        Set<String> set = new HashSet<>();
+        return dfs(set, 0, s);
+    }
+    
+    private int dfs(Set<String> set, int idx, String s) {
+        if (idx >= s.length()) return 0;
+        int res = -1;  // did not find method to split;
+        for (int i = idx + 1; i <= s.length(); i++) {
+            String sub = s.substring(idx, i);
+            if (!set.add(sub)) continue; //already contains sub
+            int next = dfs(set, i, s);
+            if (next >= 0) res = Math.max(res, next + 1);
+            set.remove(sub);
+        }
+        return res;
+    }
+}
+//https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/discuss/855073/Java-Backtracking
