aa5772f2eb0e5cc608df57e6b02140b63186e973
xxpp1314 <37097790+xxpp1314@users.noreply.github.com>
2021-07-18 23:34:24 -0700

    Create isSolvable.java

---
+++ Backtracking/1307. Verbal Arithmetic Puzzle/isSolvable.java
@@ -0,0 +1,42 @@
+class Solution {
+    private static final int[] POW_10 = new int[]{1, 10, 100, 1000, 10000, 100000, 1000000};
+    public boolean isSolvable(String[] words, String result) {
+        Set<Character> charSet = new HashSet<>();
+        int[] charCount = new int[91];
+        boolean[] nonLeadingZero = new boolean[91]; // ASCII of `A..Z` chars are in range `65..90`
+        for (String word : words) {
+            char[] cs = word.toCharArray();
+            for (int i = 0; i < cs.length; i++) {
+                if (i == 0 && cs.length > 1) nonLeadingZero[cs[i]] = true;
+                charSet.add(cs[i]);
+                charCount[cs[i]] += POW_10[cs.length - i - 1]; // charCount is calculated by units
+            }
+        }
+        char[] cs = result.toCharArray();
+        for (int i = 0; i < cs.length; i++) {
+            if (i == 0 && cs.length > 1) nonLeadingZero[cs[i]] = true;
+            charSet.add(cs[i]);
+            charCount[cs[i]] -= POW_10[cs.length - i - 1]; // charCount is calculated by units
+        }
+        boolean[] used = new boolean[10];
+        char[] charList = new char[charSet.size()];
+        int i = 0;
+        for (char c : charSet) charList[i++] = c;
+        return backtracking(used, charList, nonLeadingZero, 0, 0, charCount);
+    }
+
+    private boolean backtracking(boolean[] used, char[] charList, boolean[] nonLeadingZero, int step, int diff, int[] charCount) {
+        if (step == charList.length) return diff == 0; // difference between sum of words and result equal to 0
+        for (int d = 0; d <= 9; d++) { // each character is decoded as one digit (0 - 9).
+            char c = charList[step];
+            if (!used[d] // each different characters must map to different digits
+                    && (d > 0 || !nonLeadingZero[c])) {  // decoded as one number without leading zeros.
+                used[d] = true;
+                if (backtracking(used, charList, nonLeadingZero, step + 1, diff + charCount[c] * d, charCount)) return true;
+                used[d] = false;
+            }
+        }
+        return false;
+    }
+}
+//https://leetcode.com/problems/verbal-arithmetic-puzzle/discuss/463953/Java-Fast-Backtracking-Clean-Code-O(n!)-~-300ms
