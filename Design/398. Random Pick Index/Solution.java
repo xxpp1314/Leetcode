a10d42c2636e5bd15fc14d474a151c54c6d459f9
xxpp1314 <37097790+xxpp1314@users.noreply.github.com>
2021-08-08 22:49:36 -0700

    Create Solution.java

---
+++ Design/398. Random Pick Index/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    int[] nums;
+    Random rnd;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.rnd = new Random();
+    }
+    
+    public int pick(int target) {
+        int result = -1;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != target)
+                continue;
+            if (rnd.nextInt(++count) == 0)
+                result = i;
+        }
+        
+        return result;
+    }
+}
+// https://leetcode.com/problems/random-pick-index/discuss/88072/Simple-Reservoir-Sampling-solution
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
