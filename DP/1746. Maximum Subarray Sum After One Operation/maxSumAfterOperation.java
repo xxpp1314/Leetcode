6d76515cdffe894030d0e3d890e6d09ac2fd5e11
xxpp1314 <37097790+xxpp1314@users.noreply.github.com>
2021-08-16 10:50:06 -0700

    Create maxSumAfterOperation.java

---
+++ DP/1746. Maximum Subarray Sum After One Operation/maxSumAfterOperation.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSumAfterOperation(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = nums[0]; dp[0][1] = nums[0] * nums[0];
+        int res = dp[0][1];
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0] + nums[i], nums[i]);
+            dp[i][1] = Math.max(nums[i] * nums[i], Math.max(dp[i - 1][1] + nums[i], dp[i - 1][0] + nums[i] * nums[i]));
+            res = Math.max(res, dp[i][1]);
+        }
+        return res;
+    }
+}
+//https://leetcode.com/problems/maximum-subarray-sum-after-one-operation/discuss/1049224/Java-O(n)-Time-O(n)-Space-DP-solution
